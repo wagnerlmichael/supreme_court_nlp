468ef519f8aa8a1cff754922dd832b9eca75c8a1
wagnerlmichael <wagnerleemichael@gmail.com>
2023-04-28 14:07:26 -0500

    adding src folder, modularizing embeddings

---
+++ src/embed.py
@@ -0,0 +1,78 @@
+import torch
+import numpy as np
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from torchtext.vocab import GloVe
+from collections import Counter
+import pandas as pd
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# pre trained model for tokenizing
+nltk.download('punkt')
+
+
+def get_glove_embeddings(df_column, dim=100):
+    """
+    Inputs:
+    df_column: a single column of a data frame of textual data
+    dim: choose dim of embedding vectors
+
+    Outputs:
+    returns tensor object of embeddings
+    """
+    glove = GloVe(name='6B', dim=dim)
+
+    # set of unique words in the dataframe column, tried this to speed it up, not sure if it worked
+    # tokenize to clean formatting
+    unique_words = set(word for sentence in df_column for word in nltk.word_tokenize(sentence))
+    
+    # initialize dictionary
+    word_to_vec = {word: glove[word].numpy() for word in unique_words if word in glove.stoi}
+
+    # maps each word to embedding
+    def map_to_embedding(sentence):
+        return [word_to_vec.get(word, glove.get_vecs_by_tokens('<unk>').numpy()) 
+                for word in nltk.word_tokenize(sentence)]
+
+    # Use a new list to store embeddings instead of modifying the dataframe
+    # supposedly faster, idk
+    embeddings_list = df_column.apply(map_to_embedding).tolist()
+    #convert each list to tensor
+    embeddings = [torch.tensor(sentence) for sentence in embeddings_list]
+    # padding
+    embeddings = torch.nn.utils.rnn.pad_sequence(embeddings, batch_first=True)
+
+    return embeddings
+
+
+def get_bow_embeddings(df_column, vocab_size=10000):
+    """
+    Inputs:
+    df_column: a single column of a data frame of textual data
+    vocab_size: size of vocabulary to be used
+
+    Outputs:
+    returns tensor object of BoW representations
+    """
+    # Flatten the dataframe column into a single list of words and count the word frequencies
+    words = [word for sentence in df_column for word in nltk.word_tokenize(sentence)]
+    word_freqs = Counter(words)
+
+    # Sort the words by frequency and keep only the top vocab_size words
+    reduced_vocab = {word for word, _ in word_freqs.most_common(vocab_size)}
+
+    # Initialize a CountVectorizer object with the reduced vocabulary
+    vectorizer = CountVectorizer(vocabulary=reduced_vocab)
+
+    # Fit the vectorizer on the text data and transform the data
+    bow = vectorizer.fit_transform(df_column)
+
+    # Convert the result to a dense matrix and then to a DataFrame
+    df_bow = pd.DataFrame(bow.todense(), columns=vectorizer.get_feature_names_out())
+
+    # Convert the DataFrame to a PyTorch tensor
+    tensor_bow = torch.tensor(df_bow.values)
+
+    return tensor_bow
