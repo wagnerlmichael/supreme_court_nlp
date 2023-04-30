e3f087d5b12388450bf11906b55f6dad5a26b851
Matthew Kaufmann <matthew.e.kaufmann@gmail.com>
2023-04-30 15:56:57 -0500

    Uploading model with test df

--- src/model_helpers.py
+++ src/model_helpers.py
@@ -1,4 +1,7 @@
 import torch
+import pandas as pd
+import numpy as np
+
 
 def train_an_epoch(model, dataloader, optimizer, loss_function):
     '''
@@ -15,6 +18,9 @@ def train_an_epoch(model, dataloader, optimizer, loss_function):
         if idx % log_interval == 0 and idx > 0:
             print(f'At iteration {idx} the loss is {loss:.3f}.')
 
+    return
+
+
 def get_accuracy(model, dataloader, threshold=0.8):
     '''
     '''
@@ -24,7 +30,38 @@ def get_accuracy(model, dataloader, threshold=0.8):
         total = 0
         for _, (label, text) in enumerate(dataloader):
             log_probs = model(text).squeeze(1)
-            classifs = torch.as_tensor((log_probs - threshold) > 0, dtype=torch.int32)
-            total_correct += sum(classifs==label).item()
+            classifs = torch.as_tensor((log_probs - threshold) > 0,
+                                       dtype=torch.int32)
+            total_correct += sum(classifs == label).item()
             total += len(label)
         return total_correct / total
+
+
+def get_test_results_df(model, dataloader, test_info):
+    '''
+    Gets test results df by consolidating the case utterance probablity
+        predictions with the relevant case ids
+
+    Inputs:
+        model (model object)
+        dataloader (dataloader object)
+        test_info (pd.Dataframe)
+
+    Returns:
+        test_results (pd.Dataframe)
+    '''
+    model.eval()
+    with torch.no_grad():
+        labels = torch.empty(0)
+        probs = torch.empty(0)
+        for (label, text) in dataloader:
+            log_probs = model(text).squeeze(1)
+            probs = torch.concatenate((probs, log_probs))
+            labels = torch.concatenate((labels, label))
+        labels = labels.numpy()
+        probs = probs.numpy()
+
+        test_info['labels'] = labels
+        test_info['prob'] = probs
+
+        return test_info
--- src/pred_models.py
+++ src/pred_models.py
@@ -2,18 +2,22 @@
 
 from torch import nn
 
+
 class BoWNNClassifier(nn.Module):
     def __init__(self,
                  vocab_size,
                  hidden_dim,
                  output_dim):
- 
+
         super(BoWNNClassifier, self).__init__()
 
         self.linear1 = nn.Linear(vocab_size, hidden_dim)
         self.activation = nn.ReLU()
         self.linear2 = nn.Linear(hidden_dim, output_dim)
         self.activation2 = nn.Sigmoid()
+        self.vocab_size = vocab_size
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
 
     def forward(self, batch):
         batch = self.linear1(batch)
